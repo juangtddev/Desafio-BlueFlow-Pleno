64eec6ff34d231452150247380426985858e934b
Juan <juan.gtd.dev@gmail.com>
2025-10-17 16:05:31 -0300

    feat(favorites-service): create GET /favorites/:userId endpoint

--- services/favorites-service/src/index.ts
+++ services/favorites-service/src/index.ts
@@ -1,1 +1,32 @@
-console.log('Hello!');
+import 'dotenv/config';
+import express from 'express';
+import { prisma } from './lib/prisma';
+
+const app = express();
+app.use(express.json());
+
+app.get('/favorites/:userId', async (req, res) => {
+  try {
+    const userId = parseInt(req.params.userId, 10);
+
+    if (isNaN(userId)) {
+      return res.status(400).json({ message: 'User ID must be a number' });
+    }
+
+    const favorites = await prisma.favorite.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+
+    return res.status(200).json(favorites);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+const PORT = process.env.PORT || 3003;
+app.listen(PORT, () => {
+  console.log(`Favorites-service running on port ${PORT}`);
+});
